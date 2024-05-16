1c4d73d1f3808acfac15ffccedc131a069f1174b
Mahesh10221 <117515189+Mahesh10221@users.noreply.github.com>
2024-05-16 19:56:59 +0530

    commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <footer>
+            <p>
+                Created with ❤️ by &nbsp;
+                <a className="website-link" href="https://linktr.ee/mahesh_10221">
+                    Mahesh babu
+                </a>
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
