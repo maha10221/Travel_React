02c3bfb29093891b00e5a664f3fc5d74e872d475
maheshbabugarnepudi <160011452+maha10221@users.noreply.github.com>
2024-07-17 16:49:13 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
 import Footer from "./components/Footer";
@@ -5,14 +6,43 @@ import data from "./data";
 import "./App.css";
 
 function App() {
-    const cards = data.map((item) => {
-        return <Card key={item.id} {...item} />;
-    });
+    const cardsPerPage = 5; // Number of cards to display per page
+    const [currentPage, setCurrentPage] = useState(1);
+
+    // Logic to slice the data array based on currentPage
+    const indexOfLastCard = currentPage * cardsPerPage;
+    const indexOfFirstCard = indexOfLastCard - cardsPerPage;
+    const currentCards = data.slice(indexOfFirstCard, indexOfLastCard);
+
+    // Logic to paginate
+    const nextPage = () => {
+        if (currentPage < Math.ceil(data.length / cardsPerPage)) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
+
+    const prevPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    };
+
+    const renderCards = currentCards.map((item) => (
+        <Card key={item.id} {...item} />
+    ));
 
     return (
         <div className="App">
             <Navbar />
-            <section className="cards-list">{cards}</section>
+            <section className="cards-list">{renderCards}</section>
+            <div className="pagination">
+                <button onClick={prevPage} disabled={currentPage === 1}>
+                    Previous Page
+                </button>
+                <button onClick={nextPage} disabled={currentPage === Math.ceil(data.length / cardsPerPage)}>
+                    Next Page
+                </button>
+            </div>
             <Footer />
         </div>
     );
